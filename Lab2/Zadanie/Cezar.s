045ae188b403729b9488e1e18dc592f059f00388
Torak28 <zelechowski28@gmail.com>
2017-03-30 09:40:23 +0200

    Cezar na wypasie ale male z cos nie bangla

--- Lab2/Zadanie/Cezar.s
+++ Lab2/Zadanie/Cezar.s
@@ -37,8 +37,72 @@ jmp loop
 
 loop:
 	movb textin(,%edi,1), %bl
+	movb $0x41, %al
+	cmp %al, %bl			#Duze A - 65
+	jge litera
+	jmp nic
+litera:
+	movb $0x5a, %al
+	cmp %al, %bl			#Duze Z - 90
+	jle duza_litera
+	movb $0x61, %al
+	cmp %al, %bl			#Male a - 97
+	jge m_mala_litera
+	jmp nic
+duza_litera:
+	#KOD
+	movb $0x58, %al
+	cmp %al, %bl			#X
+	je X
+	movb $0x59, %al
+	cmp %al, %bl			#Y
+	je Y
+	movb $0x5a, %al
+	cmp %al, %bl			#Z
+	je Z
 	addb $0x2, %bl
 	movb %bl, textout(,%edi,1)
+	jmp dalej
+X:
+	movb $0x5a, textout(,%edi,1)
+	jmp dalej
+Y:
+	movb $0x41, textout(,%edi,1)
+	jmp dalej
+Z:
+	movb $0x42, textout(,%edi,1)
+	jmp dalej
+m_mala_litera:
+	movb $0x7a, %al
+	cmp %al, %bl			#Male z - 122
+	jle mala_litera
+	jmp nic
+mala_litera:
+	#KOD
+	movb $0x78, %al
+	cmp %al, %bl
+	je MX
+	movb $0x79, %al
+	cmp %al, %bl
+	je MY
+	movb $0x7a, %al
+	cmp %al, %bl
+	addb $0x2, %bl
+	movb %bl, textout(,%edi,1)
+	jmp dalej
+MX:
+	movb $0x7a, textout(,%edi,1)
+	jmp dalej
+MY:
+	movb $0x61, textout(,%edi,1)
+	jmp dalej
+MZ:
+	movb $0x62, textout(,%edi,1)
+	jmp dalej
+nic:
+	movb $0x20, textout(,%edi,1)
+	jmp dalej
+dalej:
 	inc %edi
 	cmp %edx, %edi
 	jle loop
