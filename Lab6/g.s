0d0efd26e2bccf23a76a5e8399e3d23f62744c70
Jarosalaw Ciolek-Zelechowski <zelechowski28@gmail.com>
2017-06-09 22:34:19 +0200

    5/5!

---
+++ Lab6/g.s
@@ -0,0 +1,29 @@
+.bss
+.comm result, 4
+.comm input, 4
+
+.data
+jeden: .long 1
+
+.text
+.global g
+.type g, @function
+g:
+        movss %xmm0, input
+
+        flds input      #zaladowanie argumentu do ST0
+        
+	/*
+	Operacje matematyczne
+	*/
+
+	fmul input		#^2
+	fiadd jeden		#Dodanie 1 do ST0
+	fsqrt			#pierwiastek z ST0
+	fisub jeden		#Odjecie 1	
+	
+
+        fstps result    #pobranie wyniku do pamieci
+        movss result, %xmm0     #przeniesienie arg doo zwrocenia
+
+        ret
--- Lab6/set_round.s
+++ Lab6/set_round.s
@@ -2,9 +2,6 @@
 .comm control, 2
 
 .data
-p_pojedyncza: .long 0xFCFF  	
-p_podwojna: .long 0x0200
-p_rozszerzona: .long 0x0300
 r_najblizsza: .long 0xF3FF
 r_zero: .long 0x0C00
 r_dol_o: .long 0x0400
@@ -14,69 +11,32 @@ r_gora_a: .long 0xFBFF
 
 
 .text
-.global set_fpu
-.type set_fpu, @function
+.global set_round
+.type set_round, @function
 
-set_fpu:
+set_round:
 	pushq %rbp
         movq %rsp, %rbp
 
         fstcw control
+	fwait
 
+	movq $0, %rax
         movl $0 ,%edx
         movw control(,%edx,2), %ax
 	/*W %ax jest aktualny control word*/
 
-	cmp $0, %rdi
-	je Spr_zaokrag
-	jmp dalej
-
-dalej:
-	cmp $2, %rdi
-	jl pojedyncza
-	je podwojna
-	jg rozszerzona
-
-pojedyncza:
-	movw p_pojedyncza(,%edx,2), %cx
-	andw %cx, %ax
-	movw %ax, control(,%edx,2)
-
-	/*Zapisanie control word*/
-	fldcw control
-	jmp Spr_zaokrag
-podwojna:
-	/*Pierw przygotowanie przez AND, patrz readme*/
-	movw p_pojedyncza(,%edx,2), %cx
-	andw %cx, %ax
-	movw p_podwojna(,%edx,2), %cx
-	xorw %cx, %ax
-	movw %ax, control(,%edx,2)
-	
-	/*Zapisanie control word*/
-	fldcw control
-	jmp Spr_zaokrag
-rozszerzona:
-	/*Pierw przygotowanie przez AND, patrz readme*/
-	movw p_pojedyncza(,%edx,2), %cx
-	andw %cx, %ax
-	movw p_rozszerzona(,%edx,2), %cx
-	xorw %cx, %ax
-	movw %ax, control(,%edx,2)
-	
-	/*Zapisanie control word*/
-	fldcw control
-	jmp Spr_zaokrag
+	jmp Spr_zaokrag	
 
 Spr_zaokrag:
-	cmp $3, %rsi
+	cmp $3, %rdi
 	jl zaokrag
 	je zero
-	jg koniec
+	jg najblizsza
 
 zaokrag:
-	cmp $1, %rsi
-	jl najblizsza
+	cmp $1, %rdi
+	jl koniec
 	je dol
 	jg gora
 		
@@ -121,7 +81,15 @@ gora:
 	jmp koniec
 
 koniec:
+	/*Czyszczenie na początek bo pojawiały mi się tam jakieś liczby*/
+	movq $0, %rax
+	movq $0, %rdx
+	
+	/*Odczytanie i przekazanie*/
+	fstcw control
+	fwait
+	mov control, %ax
+
 	popq %rbp
         ret
 
-	
